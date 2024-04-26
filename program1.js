bd357a658f447283c4cf911cb937e29b63acefa1
abhishekbhaware <168156302+abhishekbhaware@users.noreply.github.com>
2024-04-26 08:12:59 +0000

    Done

--- program1.js
+++ program1.js
@@ -1,9 +1,25 @@
 function smallestMissingPositiveInteger(nums) {
 
+  
+  const numSet = new Set(nums.filter(num => num > 0));
+
+
+  let smallestMissingPositive = 1;
+
+
+  while (true) {
+      if (!numSet.has(smallestMissingPositive)) {
+          return smallestMissingPositive;
+      }
+      smallestMissingPositive++;
+  }
 
     // Implement the function smallest_missing_positive_integer
   
   }
   
   module.exports = smallestMissingPositiveInteger;
-
+  // Test the function
+console.log(smallestMissingPositiveInteger([3, 4, -1, 1])); // Output: 2
+console.log(smallestMissingPositiveInteger([1, 2, 0])); // Output: 3
+console.log(smallestMissingPositiveInteger([-1, -3, 4, 2])); // Output: 1
--- program2.js
+++ program2.js
@@ -1,7 +1,40 @@
 function longestSubstring(s) {
 
+    let start = 0;
+    let end = 0;
+  
+    
+    const charSet = new Set();
+  
+   
+    let maxLength = 0;
+  
+    
+    while (end < s.length) {
+     
+      if (charSet.has(s[end])) {
+        charSet.delete(s[start]);
+        start++;
+      } else {
+        
+        charSet.add(s[end]);
+        end++;
+  
+       
+        maxLength = Math.max(maxLength, charSet.size);
+      }
+    }
+  
+    
+    return maxLength;
 
     // Implementation of longestSubstring function
 }
+
+// Test the function
+console.log(lengthOfLongestSubstring("abcabcbb"));  // Output: 3
+console.log(lengthOfLongestSubstring("bbbbb"));  // Output: 1
+console.log(lengthOfLongestSubstring("pwwkew"));  // Output: 3
+console.log(lengthOfLongestSubstring(""));  // Output: 0
 
 module.exports = { longestSubstring };
